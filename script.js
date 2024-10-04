e5f594637629aa53427ee5d6525aeed2fe76e267
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-04 22:14:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+let table=document.querySelector("#myTable");
+
+
 function createTable() {
-    //Write your code here
-  
+      let row= prompt("Input number of rows");
+	  let column= prompt("Input number of column");
+	table.innerHTML='';
+	
+	for(let i=0;i<row;i++){
+		let tr=document.createElement('tr');
+		for(let j=0;j<column;j++){
+			let td=document.createElement('td');
+			td.innerText=`Row-${i} Column-${j}`;
+			tr.appendChild(td);
+		}
+		table.appendChild(tr)
+	}
 }
